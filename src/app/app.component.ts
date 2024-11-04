f7a9cf97ecd81d334262f95497013cf75488f146
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-11-04 16:15:41 +0000

    feat: add auth feature setup and navbar func

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { AuthComponent } from './features/auth/auth.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, AuthComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'schedulo-frontend';
+  protected isAuthModalOpen = false;
+  public authMode: 'login' | 'register' = 'login';
+
+  protected onOpenLoginModal() {
+    this.authMode = 'login';
+    this.isAuthModalOpen = true;
+  }
+  protected onOpenRegisterModal() {
+    this.authMode = 'register';
+    this.isAuthModalOpen = true;
+  }
+
+  protected onCloseAAuthModal() {
+    this.isAuthModalOpen = false;
+  }
 }
