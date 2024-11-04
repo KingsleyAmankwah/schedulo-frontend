90caa407b0ba035bd82203fbf1d379312639c51e
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-11-04 09:08:37 +0000

    chore: add routes for app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./features/auth/auth.routes').then((r) => r.authRoutes),
+  },
+
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./features/admin/admin.routes').then((r) => r.adminRoutes),
+  },
+
+  {
+    path: 'client',
+    loadChildren: () =>
+      import('./features/client/client.routes').then((r) => r.clientRoutes),
+  },
+];
