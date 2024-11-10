98aadf2e1880f17755250a5edbc5f9ab34353604
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-11-10 16:23:44 +0000

    feat: setup routes

--- src/app/features/auth/auth.routes.ts
+++ src/app/features/auth/auth.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
 
-export const authRoutes: Routes = [];
+export const authRoutes: Routes = [
+  {
+    path: 'activate-account',
+    loadComponent: () =>
+      import('./activate-account/activate-account.component').then(
+        (c) => c.ActivateAccountComponent
+      ),
+  },
+
+  {
+    path: 'reset-password',
+    loadComponent: () =>
+      import('./reset-password/reset-password.component').then(
+        (c) => c.ResetPasswordComponent
+      ),
+  },
+
+  {
+    path: 'password-reset-failed',
+    loadComponent: () =>
+      import('./password-reset-failed/password-reset-failed.component').then(
+        (c) => c.PasswordResetFailedComponent
+      ),
+  },
+
+  {
+    path: 'callback',
+    loadComponent: () =>
+      import('./oauth-callback/oauth-callback.component').then(
+        (c) => c.OauthCallbackComponent
+      ),
+  },
+];
