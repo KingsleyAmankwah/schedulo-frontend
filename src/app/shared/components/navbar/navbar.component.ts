f01f829279fc3ebe81ac125bb06ac30c241badd6
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-11-10 16:31:18 +0000

    chore: refactor navbar component

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,16 +1,25 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, effect, EventEmitter, Output } from '@angular/core';
 import { CustomButtonComponent } from '../custom-button/custom-button.component';
+import { AuthService } from '../../../features/auth/services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CustomButtonComponent],
+  imports: [CustomButtonComponent, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
+  protected isLoggedIn = false;
   @Output() openLoginModal = new EventEmitter();
   @Output() openRegisterModal = new EventEmitter();
+
+  constructor(private authService: AuthService) {
+    effect(() => {
+      this.isLoggedIn = this.authService.isAuthenticated();
+    });
+  }
 
   public triggerLoginModal() {
     this.openLoginModal.emit();
