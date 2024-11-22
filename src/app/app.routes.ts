08bca6b3cd47c4f7756d9bc3feaaa2d8e6baf896
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-11-22 07:45:52 +0000

    chore: setup routes for dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,14 @@
 import { Routes } from '@angular/router';
 import { authGuard } from './core/guards/auth.guard';
+import { LayoutComponent } from './shared/components/layout/layout.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./features/home/home.component').then((c) => c.HomeComponent),
+  },
+
   {
     path: 'auth',
     loadChildren: () =>
@@ -10,10 +17,48 @@ export const routes: Routes = [
 
   {
     path: 'dashboard',
-    loadComponent: () =>
-      import('./features/dashboard/dashboard.component').then(
-        (c) => c.DashboardComponent
-      ),
+    component: LayoutComponent,
     canActivate: [authGuard],
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./features/dashboard/dashboard.component').then(
+            (c) => c.DashboardComponent
+          ),
+      },
+
+      {
+        path: 'availability',
+        loadComponent: () =>
+          import('./features/availability/availability.component').then(
+            (c) => c.AvailabilityComponent
+          ),
+      },
+
+      {
+        path: 'meetings',
+        loadComponent: () =>
+          import('./features/meetings/meetings.component').then(
+            (c) => c.MeetingsComponent
+          ),
+      },
+
+      {
+        path: 'event',
+        loadComponent: () =>
+          import('./features/events/events.component').then(
+            (c) => c.EventsComponent
+          ),
+      },
+
+      {
+        path: 'settings',
+        loadComponent: () =>
+          import('./features/settings/settings.component').then(
+            (c) => c.SettingsComponent
+          ),
+      },
+    ],
   },
 ];
