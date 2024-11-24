69d6dbc01134af9a9530a688a00b92f6530ce2b1
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-11-24 15:03:35 +0000

    chore: refactor layout component

--- src/app/shared/components/layout/layout.component.ts
+++ src/app/shared/components/layout/layout.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 import { RouterOutlet } from '@angular/router';
+import { LogoutModalComponent } from "../logout-modal/logout-modal.component";
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [SidebarComponent, RouterOutlet],
+  imports: [SidebarComponent, RouterOutlet, LogoutModalComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css',
 })
-export class LayoutComponent {}
+export class LayoutComponent {
+  showLogoutModal = false;
+
+  public triggerLogoutModal() {
+    this.showLogoutModal = !this.showLogoutModal;
+  }
+}
