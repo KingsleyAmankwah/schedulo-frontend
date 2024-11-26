b37b92569e7757cf39842511669a8a2d0a2211d2
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-11-26 10:10:07 +0000

    setup booking profile service

---
+++ src/app/features/booking-profile/services/booking.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { meeting } from '../../../shared/constants/apiEndpoints';
+import { BookingRequest } from '../types';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BookingService {
+  constructor(private http: HttpClient) {}
+
+  public scheduleMeeting(request: BookingRequest) {
+    return this.http.post<BookingRequest>(`${meeting}/book-meeting`, request);
+  }
+}
