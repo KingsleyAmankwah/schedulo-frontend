99fac5bac89c6f6dc35f0314dfbbaa1b842ee558
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-11-26 10:03:42 +0000

    chore: implement functionality for decline

--- src/app/features/meetings/components/decline-modal/decline-modal.component.ts
+++ src/app/features/meetings/components/decline-modal/decline-modal.component.ts
@@ -1,6 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { CustomButtonComponent } from '../../../../shared/components/custom-button/custom-button.component';
 import { CommonModule } from '@angular/common';
+import { MeetingService } from '../../service/meeting.service';
+import { MeetingStatus } from '../../types';
+import { response } from 'express';
 
 @Component({
   selector: 'app-decline-modal',
@@ -10,9 +13,33 @@ import { CommonModule } from '@angular/common';
   styleUrl: './decline-modal.component.css',
 })
 export class DeclineModalComponent {
+  @Input() meetingId = '';
   @Input() showModal = false;
+  @Output() meetingCancelled = new EventEmitter<string>();
+
+  private meetingService = inject(MeetingService);
+  private meetStats = MeetingStatus.Cancelled;
 
   onClose() {
-    this.showModal = !this.showModal;
+    this.showModal = false;
+  }
+  protected declineMeeting() {
+    if (!this.meetingId) {
+      console.error('No meeting ID provided');
+      return;
+    }
+    this.meetingService
+      .cancelMeeting(this.meetingId, this.meetStats)
+      .subscribe({
+        next: (response) => {
+          console.log(response);
+          this.meetingCancelled.emit(this.meetingId);
+          // this.onClose();
+        },
+
+        error: (error) => {
+          console.log(error);
+        },
+      });
   }
 }
