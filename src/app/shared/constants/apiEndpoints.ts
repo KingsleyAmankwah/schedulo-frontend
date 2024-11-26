4332894bd9cf28eefce3646d6660fc5a54169567
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-11-26 10:02:15 +0000

    chore: update api endpoints

--- src/app/shared/constants/apiEndpoints.ts
+++ src/app/shared/constants/apiEndpoints.ts
@@ -2,3 +2,6 @@ import { environment } from '../../../environments/environment';
 
 const BASE_URL = environment.BASE_URL;
 export const auth = `${BASE_URL}/auth`;
+export const events = `${BASE_URL}/events`;
+export const availability = `${BASE_URL}/availability`;
+export const meeting = `${BASE_URL}/public`;
