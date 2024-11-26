6fd548bfe852b9e8312f34e4c8ce495443aeda17
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-11-26 15:16:18 +0000

    feat: setup toast methods and initials

--- src/app/shared/services/shared.service.ts
+++ src/app/shared/services/shared.service.ts
@@ -1,4 +1,12 @@
 import { Injectable, signal } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { generateGuid } from '../utils';
+
+export interface Toast {
+  id: string;
+  message: string;
+  type: 'success' | 'error' | 'info' | 'warning';
+}
 
 @Injectable({
   providedIn: 'root',
@@ -12,5 +20,35 @@ export class SharedService {
   }
   public closeLoginModal() {
     this._showLoginModal.set(false);
+  }
+
+  public toasts: BehaviorSubject<Toast[]> = new BehaviorSubject<Toast[]>([]);
+  public successToastr(message: string) {
+    this.showToast({ message, type: 'success', id: generateGuid() });
+  }
+
+  public errorToastr(message: string) {
+    this.showToast({ message, type: 'error', id: generateGuid() });
+  }
+
+  public infoToastr(message: string) {
+    this.showToast({ message, type: 'info', id: generateGuid() });
+  }
+
+  public warningToastr(message: string) {
+    this.showToast({ message, type: 'warning', id: generateGuid() });
+  }
+
+  public closeToast(id: string) {
+    this.toasts.next(this.toasts.value.filter((t) => t.id !== id));
+  }
+
+  private removeToast(toast: Toast) {
+    setTimeout(() => this.closeToast(toast.id), 5000);
+  }
+
+  private showToast(toast: Toast) {
+    this.toasts.next([...this.toasts.value, toast]);
+    this.removeToast(toast);
   }
 }
