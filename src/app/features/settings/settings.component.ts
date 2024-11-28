5f59e189215488161ca2bfb77d9d6e9bf7cf7c88
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-11-28 14:41:32 +0000

    settings component

--- src/app/features/settings/settings.component.ts
+++ src/app/features/settings/settings.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './settings.component.html',
-  styleUrl: './settings.component.css'
+  styleUrl: './settings.component.css',
 })
 export class SettingsComponent {
+  settingsForm: FormGroup;
+  notifications = true;
 
+  constructor(private fb: FormBuilder) {
+    this.settingsForm = this.fb.group({
+      firstName: [''],
+      lastName: [''],
+      email: [''],
+      workHoursStart: ['09:00'],
+      workHoursEnd: ['17:00'],
+    });
+  }
+
+  toggleNotifications() {
+    this.notifications = !this.notifications;
+  }
+
+  saveChanges() {
+    if (this.settingsForm.valid) {
+      console.log('Settings saved:', this.settingsForm.value);
+    }
+  }
 }
