3191cd912d7faa0492cb8996ceac46e53e199894
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-12-03 11:42:08 +0000

    chore: add home component

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,12 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
+interface Feature {
+  icon: string;
+  title: string;
+  description: string;
+}
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
+  features: Feature[] = [
+    {
+      icon: 'clock',
+      title: 'Easy Booking',
+      description:
+        'Intuitive interface to schedule sessions in just a few clicks.',
+    },
+    {
+      icon: 'users',
+      title: 'Connect Anywhere',
+      description:
+        'Book sessions with people from around the globe effortlessly.',
+    },
+    {
+      icon: 'calendar',
+      title: 'Smart Management',
+      description: 'Intelligent calendar integration and time slot management.',
+    },
+  ];
 }
