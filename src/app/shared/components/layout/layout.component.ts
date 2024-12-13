b9eb67aec60689b91058e861d7b3d758db94ad45
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-12-13 10:08:18 +0000

    ...

--- src/app/shared/components/layout/layout.component.ts
+++ src/app/shared/components/layout/layout.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 import { RouterOutlet } from '@angular/router';
 import { LogoutModalComponent } from '../logout-modal/logout-modal.component';
+import { SharedService } from '../../services/shared.service';
 
 @Component({
   selector: 'app-layout',
@@ -11,6 +12,7 @@ import { LogoutModalComponent } from '../logout-modal/logout-modal.component';
   styleUrl: './layout.component.css',
 })
 export class LayoutComponent {
+  protected sharedService = inject(SharedService);
   showLogoutModal = false;
   public sidebarExpanded = false;
 
