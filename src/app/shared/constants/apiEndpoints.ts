79d6b2972afd839518867f959aef388ae47fbfcc
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-12-15 17:43:30 +0000

    chore: add user endpoint

--- src/app/shared/constants/apiEndpoints.ts
+++ src/app/shared/constants/apiEndpoints.ts
@@ -5,3 +5,4 @@ export const auth = `${BASE_URL}/auth`;
 export const events = `${BASE_URL}/events`;
 export const availability = `${BASE_URL}/availability`;
 export const meeting = `${BASE_URL}/public`;
+export const user = `${BASE_URL}/user`;
